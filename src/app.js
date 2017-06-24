1b22604a5af6e429816a1d189bf4830a64c4f385
Martin Banks <anotherbanksy@gmail.com>
2017-06-24 12:18:12 +1000

    render results through pug template

--- src/app.js
+++ src/app.js
@@ -3,32 +3,44 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const path = require('path')
+const pug = require('pug')
 
 const dir = path.join(__dirname, '../../')
+const excludeFiles = [
+// any file beginning with a full-point will be excluded
+	'node_modules',
+]
 
+// const testPug = pug.compileFile('./src/test.pug')
+// console.log(testPug({name: 'bob'}))
+
+app.set('view engine', 'pug')
+app.set('views', './src/views')
 
 app.get('/*', (req, res) => {
 	console.log(Object.keys(req), req.url, req.params)
-	const { referer } = req.headers
-	console.log('-----\n', referer, req.originalUrl, '\n---------')
+
 	if (req.url.indexOf('.') === -1) {
 		fs.readdir(path.join(dir, req.originalUrl), (err, data) => {
 			if (err) {
-				console.log(err)
+				console.error(err)
+				res.send(error)
 				return
 			}
-			const dataString = JSON.stringify(data, 'utf8', '\t')
 			const formatted = data
 				.filter(d => d[0] !== '.')
-				.map(d => `<li><a href="http://${req.hostname}:3000${req.originalUrl.replace(/\/+/g, '/')}/${d}">${d}</a></li>`).join('')
+				// .map(d => `<li><a href="http://${req.hostname}:3000${req.originalUrl.replace(/\/+/g, '/')}/${d}">${d}</a></li>`).join('')
+				.map(d => ({
+					text: d,
+					link: `http://${req.hostname}:3000${req.originalUrl.replace(/\/+/g, '/')}/${d}`}))
 
-			res.send(`<html><body><ul>
-					${formatted}
-				</ul></body></html>`)
+			res.render('test', { 
+				header: `Files for ${req.originalUrl}`,
+				files: formatted
+			})
 		})
 	} else {
 		const file = path.join(dir, req.url)
-		console.log(file)
 		res.sendFile(file)
 	}
 })
