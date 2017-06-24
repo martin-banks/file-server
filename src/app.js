e9bca35f92fc2408245028e31c675fd3659ae1f2
Martin Banks <anotherbanksy@gmail.com>
2017-06-24 13:36:08 +1000

    add sass support with dynamic rendering

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 /* eslint-env node */
 const express = require('express')
+const sassMiddleware = require('node-sass-middleware')
 const app = express()
 const fs = require('fs')
 const path = require('path')
-const pug = require('pug')
+// const pug = require('pug')
 
 const dir = path.join(__dirname, '../../')
 const excludeFiles = [
@@ -17,26 +18,38 @@ const excludeFiles = [
 app.set('view engine', 'pug')
 app.set('views', './src/views')
 
-app.get('/*', (req, res) => {
-	console.log(Object.keys(req), req.url, req.params)
+app.use(sassMiddleware({
+	src: path.join(dir, '/file-server/src/sass'),
+	dest: path.join(dir, '/file-server/src/public'),
+	debug: true,
+	indentedSyntax: true,
+	outputStyle: 'compressed',
+	prefix: '/public',
+}))
 
+
+app.use('/public', express.static(path.join(__dirname, '/public')))
+
+
+app.get('/*', (req, res) => {
+	// console.log(Object.keys(req), req.url, req.params)
 	if (req.url.indexOf('.') === -1) {
 		fs.readdir(path.join(dir, req.originalUrl), (err, data) => {
 			if (err) {
 				console.error(err)
-				res.send(error)
+				res.send(err)
 				return
 			}
 			const formatted = data
 				.filter(d => d[0] !== '.')
-				// .map(d => `<li><a href="http://${req.hostname}:3000${req.originalUrl.replace(/\/+/g, '/')}/${d}">${d}</a></li>`).join('')
 				.map(d => ({
 					text: d,
 					link: `http://${req.hostname}:3000${req.originalUrl.replace(/\/+/g, '/')}/${d}`}))
 
-			res.render('test', { 
+			res.render('test', {
 				header: `Files for ${req.originalUrl}`,
-				files: formatted
+				files: formatted,
+				style: path.join(__dirname, 'public'),
 			})
 		})
 	} else {
@@ -46,5 +59,5 @@ app.get('/*', (req, res) => {
 })
 
 app.listen(3000, () => {
-  console.log('App listening on port 3000!')
+	console.log('App listening on port 3000!', path.join(dir, '/file-server/src/sass'))
 })
